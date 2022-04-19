7ec707eb4f617a981c1315a8f0cda00958271103
kanekodaichi <knkdic.bfc@gmail.com>
2022-04-19 23:52:59 +0900

    ページネーションを追加

--- app/controllers/footballers_controller.rb
+++ app/controllers/footballers_controller.rb
@@ -1,25 +1,20 @@
 class FootballersController < ApplicationController
   before_action :set_footballer, only: %i[ show edit update destroy ]
 
-  # GET /footballers
   def index
-    @footballers = Footballer.all
+    @footballers = Footballer.page(params[:page])
   end
 
-  # GET /footballers/1
   def show
   end
 
-  # GET /footballers/new
   def new
     @footballer = Footballer.new
   end
 
-  # GET /footballers/1/edit
   def edit
   end
 
-  # POST /footballers
   def create
     @footballer = Footballer.new(footballer_params)
 
@@ -30,7 +25,6 @@ class FootballersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /footballers/1
   def update
     if @footballer.update(footballer_params)
       redirect_to @footballer, notice: "Footballer was successfully updated."
@@ -39,19 +33,16 @@ class FootballersController < ApplicationController
     end
   end
 
-  # DELETE /footballers/1
   def destroy
     @footballer.destroy
     redirect_to footballers_url, notice: "Footballer was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_footballer
       @footballer = Footballer.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def footballer_params
       params.require(:footballer).permit(:name, :age)
     end
